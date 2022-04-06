6121869fad92531a558104688f2832f0a85aa2b1
Miki Patel <88789894+abitdrag@users.noreply.github.com>
2022-04-06 14:51:19 +0530

    Trade logic added

--- Solution/Project/helper.cpp
+++ Solution/Project/helper.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #include<string>
 using namespace std;
-
+int order_count = 0;
 string item_names[] = {
 	"GOLD",
 	"SILVER",
@@ -15,7 +15,7 @@ string item_names[] = {
 	"BAHT",
 };
 
-string trader_names[] = {
+string tader_names[] = {
 	"n1",
 	"n2",
 	"n3",
@@ -81,15 +81,32 @@ vector<Trader*> get_loggedin_traders(){
 	return loggedin_traders;
 }
 
+// Represents one order
 class Order
 {
 	Trader *trader, *match;
 	Item *item;
 	int orderId, quantity, price;
 	char type;
-	//char status;
+	// to store the remaining quantity of order after partial filling
+	int remaining_quantity;
+	// status: F = FILLED, Q = In queue
+	char status;
 
 	public:
+
+	// Always use this constructor
+	Order(Trader *tra, Item *i, int quan, int pri, char t){
+		trader = tra;
+		item = i;
+		orderId = ++order_count;
+		quantity = quan;
+		price = pri;
+		type = t;
+		remaining_quantity = quan;
+		status = 'Q';
+	}
+
 	void set_trader(Trader *t){trader = t;}
 	void set_match(Trader *m){match = m;}
 	void set_item(Item *i){item = i;}
@@ -97,7 +114,10 @@ class Order
 	void set_quantity(int q){quantity = q;}
 	void set_price(int p){price = p;}
 	void set_type(char c){type = c;}
+	void set_remaining_quantity(int q){remaining_quantity = q;}
+	void set_status(char c){status = c;}
 
+	char get_status(){return status;}
 	Trader* get_trader(){return trader;}
 	Trader* get_match(){return match;}
 	int get_orderid(){return orderId;}
@@ -105,10 +125,11 @@ class Order
 	int get_quantity(){return quantity;}
 	int get_price(){return price;}
 	char get_type(){return type;}
+	int get_remaining_quantity(){return remaining_quantity;}
 
 };
 
-
+// Represents trading entity
 class Item
 {
 	int itemId, price;
@@ -164,32 +185,103 @@ class Item
 		trade_orders();
 	}
 
-	// TO DO: 
 	// perform trade 
 	void trade_orders(){
 		// check whether trade can be performed or not
 		if(buy_book.size() > 0 && sell_book.size() > 0){
 			// check if top matches -- top is at index 0
 			// CASE 1: TOP OF BUY AND SELL BOOK MATCH
-			////////////////////////////////////////////////////
+			///////////////////////////////////////////////////////////
 			//			BUY				SELL
-			//-----------------------------------------------
+			//---------------------------------------------------------
 			//			10				10
 			//			9				11
-			if(buy_book[0]->get_price() == sell_book[0]->get_price()){
-				// proper match
+			// CASE 2: TOP OF BUY IS MORE THAN TOP OF SELL
+			///////////////////////////////////////////////////////////
+			//			BUY				SELL
+			//---------------------------------------------------------
+			//			14				11
+			//			9				12
+			//			7				13
+			//			6				16
+			if(buy_book[0]->get_price() >= sell_book[0]->get_price()){
+				// proper price match
+				// set current price
+				price = buy_book[0]->get_price();
+				// check quantity
+				if(buy_book[0]->get_remaining_quantity() == sell_book[0]->get_remaining_quantity()){
+					// BOTH HAVE SAME QUANTITY
+					// set the match of order
+					buy_book[0]->set_match(sell_book[0]->get_trader());
+					sell_book[0]->set_match(buy_book[0]->get_trader());
+					buy_book[0]->set_status('F');
+					sell_book[0]->set_status('F');
+					buy_book[0]->set_remaining_quantity(0);
+					sell_book[0]->set_remaining_quantity(0);
+					// remove both orders
+					buy_book.erase(buy_book.begin());
+					sell_book.erase(sell_book.begin());
+				}
+				else if(buy_book[0]->get_remaining_quantity() > sell_book[0]->get_remaining_quantity()){
+					// TOP OF THE BUY BOOK HAS MORE QUANTITY
+					// set match of sell order
+					sell_book[0]->set_match(buy_book[0]->get_trader());
+					sell_book[0]->set_status('F');
+					// update quantity of buy order
+					buy_book[0]->set_remaining_quantity(buy_book[0]->get_remaining_quantity() - sell_book[0]->get_remaining_quantity());
+					sell_book[0]->set_remaining_quantity(0);
+					// remove sell order
+					sell_book.erase(sell_book.begin());
+				}
+				else{
+					// TOP OF SELL BOOK HAS MORE QUANTITY
+					// set match of buy order
+					buy_book[0]->set_match(sell_book[0]->get_trader());
+					buy_book[0]->set_status('F');
+					// update quantity of sell order
+					sell_book[0]->set_remaining_quantity(sell_book[0]->get_remaining_quantity() - buy_book[0]->get_remaining_quantity());
+					buy_book[0]->set_remaining_quantity(0);
+					// remove buy order
+					buy_book.erase(buy_book.begin());
+				}
+				// recursive call to see if other orders can match or not
+				trade_orders();
 			}
-			// CASE 2: BUY MATCHES WITH MANY SELLS
-			else if(){
-				
-			}
-			// case 3: SELL MATCHES WITH MANY BUYS
-			else if(){
-				
+			// CASE 3: SELL AND BUY DONT MATCH
+			///////////////////////////////////////////////////////////
+			//			BUY				SELL
+			//---------------------------------------------------------
+			//			10				12
+			//			9				14
+			//			7				16
+			else {
+				// TRADE IS NOT POSSIBLE
+				// EXIT
 			}
 		}
 	}
 
+	// This function prints buy and sell orders of item
+	void print_orders(){
+		cout << "********************************************************************************" << endl;
+		cout << "ORDER BOOK FOR " << itemName << endl;
+		cout << "/////////////////////////////////////////////////////////////" << endl;
+		cout << "\t\tBUY PRICE(Quantity)\t\tSELL PRICE(Quantity)" << endl;
+		cout << "/////////////////////////////////////////////////////////////" << endl;
+		for(int i=0; i<buy_book.size() || i<sell_book.size(); i++){
+			if(i < buy_book.size()){
+				cout << "\t\t\t" << buy_book[i]->get_price() << "(" << buy_book[i]->get_quantity() << ")\t\t";  
+			}
+			else{
+				cout << "\t\t\t\t\t\t\t";
+			}
+			if(i < sell_book.size()){
+				cout << "\t\t\t" << sell_book[i]->get_price() << "(" << sell_book[i]->get_quantity() << ")";  
+			}
+			cout << endl;
+		}
+		cout << "********************************************************************************" << endl;
+	}
 };
 // List of all items
 vector<Item*> items;
@@ -204,7 +296,10 @@ void init_items(){
 	}
 }
 
-
+// HEAP NOT NEEDED
+// AS BOTH ORDER BOOKS ARE SORTED
+// AND AT A TIME WE ARE INSERTING ONLY ONE ORDER
+// USING INSERTION SORT
 class Heap
 {
 private:
